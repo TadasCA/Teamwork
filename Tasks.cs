036bc5bc9482feca4c392dd6aae8c8efbcfa94a1
VytautasSts <vytas.sts@gmail.com>
2022-11-17 18:42:01 +0200

    First and second tasks done

---
+++ Tasks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teamwork
+{
+    internal class Tasks
+    {
+        public void task1()
+        {
+            double Diameter  = 6371;
+            double Radius = Diameter / 2;
+            double SurfaceArea = 4*Math.PI*Math.Pow(2, Radius);
+
+        }
+        public void task2()
+        {
+            double Diameter = 6371;
+            double Radius = Diameter / 2;
+            double Volume = 4/3 * Math.PI * Math.Pow(3, Radius);
+
+        }
+    }
+}
