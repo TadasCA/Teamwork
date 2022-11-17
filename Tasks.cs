b016c924e57913e6a8130e090631fe1842feb363
TadasCA <tadas.naudziunas@gmail.com>
2022-11-17 19:22:54 +0200

    Final touches

--- Tasks.cs
+++ Tasks.cs
@@ -10,19 +10,19 @@ namespace Teamwork
     {
         public double task1()
         {
-            double Diameter  = 6371;
-            double Radius = Diameter / 2;
-            double SurfaceArea = 4*Math.PI*Math.Pow(2, Radius);
-            return (SurfaceArea);
+            int Diameter  = 6371;
+            int Radius = Diameter / 2;
+            double SurfaceArea = (4*Math.PI*Math.Pow(2, Radius));
+            return SurfaceArea;
         }
 
         public double task2()
 
         {
-            double Diameter = 6371;
-            double Radius = Diameter / 2;
-            double Volume = 4 / 3 * Math.PI * Math.Pow(3, Radius);
-            return (Volume);
+            int Diameter = 6371;
+            int Radius = Diameter / 2;
+            double Volume = (4 / 3 * Math.PI * Math.Pow(3, Radius));
+            return Volume;
         }
 
         public double task3()
@@ -35,13 +35,13 @@ namespace Teamwork
         }
         public void task4()
         {
-            double SurfaceArea = 1;
-            double Volume = 1;
-            int izambine = 1;
+            double SurfaceArea = task1();
+            double Volume = task2();
+            double izambine = task3();
             Console.WriteLine("**********************************");
             Console.WriteLine("* Viso kintamųjų: 3");
-            Console.WriteLine("* SurfaceArea: {0}", SurfaceArea);
-            Console.WriteLine("* Volume: {0}", Volume);
+            Console.WriteLine("* SurfaceArea: ", SurfaceArea);
+            Console.WriteLine("* Volume: ", Volume);
             Console.WriteLine("* izambine: {0}", izambine);
             Console.WriteLine("* Suma: " + SurfaceArea + Volume + izambine);
             Console.WriteLine("* Vidurkis: " + (SurfaceArea + Volume + izambine) / 3);
