0d3c0247c914a94e30f15b88b75f763370f2baf8
TadasCA <tadas.naudziunas@gmail.com>
2022-11-17 18:25:02 +0200

    Main method created

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teamwork
+{
+    class Execution
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
